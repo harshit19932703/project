41ca9eb9f340ed1eaa5b53d46de5c35e8a2bf729
Harshit Sharma <harshit19932703@gmail.com>
2018-10-06 15:46:31 +0530

    updated code

--- src/Components/FormComponent/index.js
+++ src/Components/FormComponent/index.js
@@ -10,7 +10,8 @@ class FormComponent extends Component {
     this.state = {
       tag1: null,
       tag2: null,
-      data: null
+      data: null,
+      isLoad:true,
     };
   }
   handleChange = ({ target }) => {
@@ -18,23 +19,42 @@ class FormComponent extends Component {
       [target.name]: target.value
     });
   };
+  enterPressed(event) {
+    var code = event.keyCode || event.which;
+    if(code === 13) { 
+      this.sendtags()
+    } 
+}
   sendtags = () => {
     if (this.state.tag1 && this.state.tag2) {
-      this.fetchdata();
+      this.fetchdata()
     } else {
       alert("Fill Both the Tags To get Search Results");
     }
   };
   async fetchdata() {
     const { tag1, tag2 } = this.state;
+    this.setState({isLoad:false})
     var response = await fetch(
       `https://api.github.com/search/commits?q=${tag1} ${tag2}`,
       { headers: { Accept: "  application/vnd.github.cloak-preview" } }
     );
     var data = await response.json();
-    this.setState({ data: data });
+    this.setState({ data: data});
   }
   render() {
+    var LIST;
+    if(this.state.isLoad && this.state.data == null){
+      LIST=<div className="nodata">ENTER TAGS TO BEGIN SEARCH</div>
+    }
+    if(this.state.isLoad == false)
+    {
+      LIST=<div className="nodata">FETCHING DATA.....</div>
+    }
+    if(this.state.isLoad == false && this.state.data !=null){
+      LIST=<ListComponent data={this.state.data}></ListComponent>
+    }
+    
     return (
       <Fragment>
         <Row className="container">
@@ -48,6 +68,7 @@ class FormComponent extends Component {
                 placeholder="Type a Tag"
                 name="tag1"
                 onChange={this.handleChange}
+                onKeyPress={this.enterPressed.bind(this)}
               />
             </InputGroup>
           </Col>
@@ -58,6 +79,7 @@ class FormComponent extends Component {
                 placeholder="Type a Tag"
                 name="tag2"
                 onChange={this.handleChange}
+                onKeyPress={this.enterPressed.bind(this)}
               />
             </InputGroup>
           </Col>
@@ -68,7 +90,7 @@ class FormComponent extends Component {
           </Col>
         </Row>
 
-        <ListComponent data={this.state.data} />
+        {LIST}
       </Fragment>
     );
   }
--- src/Components/ListComponent/index.js
+++ src/Components/ListComponent/index.js
@@ -11,42 +11,43 @@ import {
 import "./style.css";
 
 class ListComponent extends Component {
-  constructor(p) {
-    super(p);
-  }
 
   render() {
     const mydata = (this.props.data || []).items || [];
     var content;
-    if (this.props.data === null) {
-      content = <div className="nodata">ENTER TAGS TO BEGIN SEARCH</div>;
-    } else if (this.props.data) {
-      var len = ((this.props.data || {}).items || []).length;
-      if (len >= 1) {
-        content = mydata.map((item, i) => {
-          if (item.author != null) {
-            var dp = item.author.avatar_url;
-          }
-          return (
-            <Col md="3" key={i}>
-              <Card>
-                <a href={item.author ? item.author.html_url : ''} target="_blank"><CardImg top width="100px" src={dp} alt="Github Image Link" /></a>
-                <CardBody>
-                <a href={item.author ? item.author.html_url : ''} target="_blank"> <CardTitle>Author : {item.commit.author.name} </CardTitle></a>
-                  <CardSubtitle>
-                    Committer: {item.commit.committer.name}
-                  </CardSubtitle>
-                  <CardText>CommitMessage: {item.commit.message}</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-          );
-        });
-      } else {
-        content = (
-          <div className="nodata">NO DATA FOR THESE TAGS.TRY AGAIN!!</div>
+    var len = ((this.props.data || {}).items || []).length;
+    if (len >= 1) {
+      content = mydata.map((item, i) => {
+        if (item.author != null) {
+          var dp = item.author.avatar_url;
+        }
+        return (
+          <Col md="3" key={i}>
+            <Card>
+              <a href={item.author ? item.author.html_url : ""} target="_blank" rel="noopener noreferrer">
+                <CardImg top width="100px" src={dp} alt="Github Image Link" />
+              </a>
+              <CardBody>
+                <a
+                  href={item.author ? item.author.html_url : ""}
+                  target="_blank" rel="noopener noreferrer"
+                >
+                  {" "}
+                  <CardTitle>Author : {item.commit.author.name} </CardTitle>
+                </a>
+                <CardSubtitle>
+                  Committer: {item.commit.committer.name}
+                </CardSubtitle>
+                <CardText>CommitMessage: {item.commit.message}</CardText>
+              </CardBody>
+            </Card>
+          </Col>
         );
-      }
+      });
+    } else {
+      content = (
+        <div className="nodata">NO DATA OR SOMETHING WENT WRONG.TRY AGAIN!!</div>
+      );
     }
     return <div className="cardclass">{content}</div>;
   }
