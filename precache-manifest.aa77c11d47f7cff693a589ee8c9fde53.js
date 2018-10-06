28bc66d786e16296181fe988e6e4ad8379aa62f0
Harshit Sharma <harshit19932703@gmail.com>
2018-10-06 17:00:21 +0530

    Updates

---
+++ precache-manifest.aa77c11d47f7cff693a589ee8c9fde53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e733734cfc14039553bf",
+    "url": "/project/static/js/runtime~main.e733734c.js"
+  },
+  {
+    "revision": "d1c8d49d3d7e28ec4b7b",
+    "url": "/project/static/js/main.d1c8d49d.chunk.js"
+  },
+  {
+    "revision": "4fbe562a42f63e4b9aa3",
+    "url": "/project/static/js/1.4fbe562a.chunk.js"
+  },
+  {
+    "revision": "d1c8d49d3d7e28ec4b7b",
+    "url": "/project/static/css/main.4cfcd7c6.chunk.css"
+  },
+  {
+    "revision": "4fbe562a42f63e4b9aa3",
+    "url": "/project/static/css/1.34e15577.chunk.css"
+  },
+  {
+    "revision": "d0b7492b0b1df69677ba9602127c44ce",
+    "url": "/project/index.html"
+  }
+];
